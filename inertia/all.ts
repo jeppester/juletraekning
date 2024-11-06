f39974b83bf77fc3b096d8312c39aa07a5377821
Jesper Sørensen <jeppester1@gmail.com>
2024-11-06 21:53:46 +0000

    Remove unused boiler plate

--- inertia/all.ts
+++
@@ -1,6 +0,0 @@
-// NEEDED FOR FULL COVERAGE
-// The front-end istanbul coverage setup can only provide coverage for files built by vite.
-// This file - which we only include in our test build - imports all FE files, so that we get
-// coverage even for unused files
-
-import.meta.glob(['./**/*.tsx', '!**/*test.tsx'], { eager: true })
--- tests/support/timestamps.ts
+++
@@ -1,8 +0,0 @@
-type TimestampType = 'createdAt' | 'updatedAt'
-
-export default function timestamps<T extends TimestampType>(
-  types: T[] = ['createdAt', 'updatedAt'] as T[]
-): Record<T, Date> {
-  const entries = types.map((type) => [type, new Date()])
-  return Object.fromEntries(entries)
-}
